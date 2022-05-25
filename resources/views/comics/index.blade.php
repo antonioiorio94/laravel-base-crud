16e359e2ab30b441a99ed65f70ff75ed14015b29
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-25 17:36:51 +0200

    fix link di index per show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,7 +3,7 @@
   <h3>{{$comic->title}}</h3>
   <h4>{{$comic->series}}</h4>
   <h4>{{$comic->price}}</h4>
-  <a href="{{route('comics', $comic->id)}}">Visualizza dettagli</a>
+  <a href="{{route('comics.show', $comic->id)}}">Visualizza dettagli</a>
 </div>
 
 @empty
